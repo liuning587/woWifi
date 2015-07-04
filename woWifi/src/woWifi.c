ba16a6c74fa19e78336a553f55a86cb4f5a87d3f
liuning <liuning@liuning-PC>
2015-07-05 00:14:53 +0800

    清除调试信息

--- woWifi/src/woWifi.c
+++ woWifi/src/woWifi.c
@@ -5,7 +5,7 @@
  * @details   This file including all API functions's 
  *            implement of woWifi.c.	
  *
- * @copyright Copyrigth(C), 2008-2012,Sanxing Electric Co.,Ltd.
+ * @copyright 
  ******************************************************************************
  */
  
